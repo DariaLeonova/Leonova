3b06258ed1799e09a90d3deb20e95f6bc9ad717f
DariaLeonova <leonovadaria18@gmail.com>
2022-09-15 13:56:49 +0300

    first commit

--- cypress/e2e/specQA.cy.js
+++ cypress/e2e/specQA.cy.js
@@ -4,7 +4,7 @@ describe('window SignIn', () => {
     cy.get('h2').invoke('text').should('eq',"Auf Facebook bleibst du mit Menschen in Verbindung und teilst Fotos, Videos und vieles mehr mit ihnen.");
 
   cy.get('input[name="email"]').type('leonova@gmail.com');
-  cy.get('[name="pass"]').type('123456qqq');
+  cy.get('[name="pass"]').type('123456qqqqqqqq');
 
   cy.get('[name="email"]').invoke('val').should('eq',"leonova@gmail.com");
   cy.get('[name="pass"]').invoke('val').should('eq',"123456qqq");
