a22a7958a4af429398c3b853b83957271554bb74
DariaLeonova <leonovadaria18@gmail.com>
2022-09-21 15:23:17 +0300

    hw11

--- cypress/e2e/11Test/11TestLeonova1.cy.js
+++ cypress/e2e/11Test/11TestLeonova1.cy.js
@@ -1,18 +1,19 @@
 describe('Authorization', () => {
     it('check ', () => {
-      cy.viewport(1500, 900);
-      cy.visit('https://yevheniiahlovatska.editorx.io/web-practice/',{ timeout: 5000 });
+    
+      cy.visit('https://yevheniiahlovatska.editorx.io/web-practice/').wait(10000);
 
   
-      cy.get('#comp-kbgakxea #comp-kbgakxea_r_comp-kbgajy18').find('[class="_1UDJF"]').click(),{ timeout: 5000 };
+      cy.contains('Log In').click().wait(10000);
      
-      cy.get('#SM_ROOT_COMP9').should('be.visible');
+      cy.contains('Sign up with email').click()
+     
+      cy.get('#emailInput_SM_ROOT_COMP9').click().type('leonovadaria18@gmail.com');
+      cy.get('#input_input_passwordInput_SM_ROOT_COMP9').click().type('12345@A');
 
-      cy.get('div[data-testid="switchToEmailLink"]>button').click();
-      cy.get('#SM_ROOT_COMP9').should('be.visible');
-      cy.get('#input_input_emailInput_SM_ROOT_COMP9').should('have.value', 'leonovadaria18@gmail.com');
-      cy.get('#input_input_passwordInput_SM_ROOT_COMP9').should('have.value', '123456A@');
-      cy.get('#okButton_SM_ROOT_COMP9 button').click();
+      cy.get('[data-testid="buttonElement"]').click('center');
+     
+      
 
 
     })  
--- cypress/e2e/11Test/11TestLeonova2.cy.js
+++ cypress/e2e/11Test/11TestLeonova2.cy.js
@@ -2,15 +2,15 @@ describe('Comment submission form', () => {
     it('check ', () => {
     
       cy.visit('https://yevheniiahlovatska.editorx.io/web-practice/post/what-to-wear-to-a-blacktie-event');
-      cy.get('[class="YCaN2C blog-card-border-color blog-card-background-color"]').scrollIntoView({duration: 3000});
-      cy.wait(5000);
+      cy.scrollTo(0,2200);
+      //cy.get('#root-comment-box-start').scrollIntoView;
+      cy.wait(30000);
 
-      cy.contains('button[data-hook="comment-box-placeholder"] ').click();
-      cy.get('div[class="public-DraftStyleDefault-block public-DraftStyleDefault-ltr"]').should('have.value', 'hello');
-      cy.contains('XTZ / BTC').should('be.visible');
-      cy.contains('div[data-hook="primary-btn"]').click();
+      cy.contains('Write a comment...').should('be.visible');
+      cy.get('[class="CGV7a3 wc-border-color-box wc-corner-radius-box wc-border-width"]').click().type('Test');
+      cy.get('div[data-hook="primary-btn"]').click();
 
-      cy.contains('hello').should('be.visible')
+      
 
       
 
