d70ae6c5d5a8168b7d5ef95cff12af9bdbe9b2e2
shadowlightcode <59807230+shadowlightcode@users.noreply.github.com>
2022-09-26 09:37:39 +0300

    Update 11TestLeonova1.cy.js

--- cypress/e2e/11Test/11TestLeonova1.cy.js
+++ cypress/e2e/11Test/11TestLeonova1.cy.js
@@ -1,20 +1,15 @@
+/*1) #input_input_passwordInput_SM_ROOT_COMP9. - не  уверенна по айди что он будет работать всегда, 
+можно было взять вариант - [id*='input_input_passwordInput_SM_ROO']
+*/
 describe('Authorization', () => {
     it('check ', () => {
     
       cy.visit('https://yevheniiahlovatska.editorx.io/web-practice/').wait(10000);
-
-  
       cy.contains('Log In').click().wait(10000);
-     
       cy.contains('Sign up with email').click()
-     
       cy.get('#emailInput_SM_ROOT_COMP9').click().type('leonovadaria18@gmail.com');
       cy.get('#input_input_passwordInput_SM_ROOT_COMP9').click().type('12345@A');
-
       cy.get('[data-testid="buttonElement"]').click('center');
-     
-      
-
 
     })  
 })
